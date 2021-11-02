3285db51fac96bac01313d380bc0de8f0edc9274
Ben <benbperry1@gmail.com>
2021-11-01 17:37:38 -0700

    commit tree 825ded6ae5083b73692ecbf4a8a0710774f69eac

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 =104
+let num4 =100
+
+let mod = num3 % num4
+
+let max = (1,2,20)
+Math.max(max)
